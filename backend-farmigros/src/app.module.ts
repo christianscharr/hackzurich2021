3453749934cde00de0803635ac095f8eccc330c0
Christian Scharr <christian.scharr@gmail.com>
2021-09-25 02:03:19 +0200

    add mongodb connection

--- backend-farmigros/src/app.module.ts
+++ backend-farmigros/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Credentials } from './credentials';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(Credentials.mongoDbUrl)],
   controllers: [AppController],
   providers: [AppService],
 })
