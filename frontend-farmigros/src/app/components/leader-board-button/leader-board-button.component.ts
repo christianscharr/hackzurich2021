5c30b518e7c34b843b8e3676781de3f5c4c20c45
lenoxy <lenoxy@gmx.ch>
2021-09-25 23:39:37 +0200

    leaderboard prettify

--- frontend-farmigros/src/app/components/leader-board-button/leader-board-button.component.ts
+++ frontend-farmigros/src/app/components/leader-board-button/leader-board-button.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {PopoverController} from "@ionic/angular";
-import {TutorialComponent} from "../tutorial/tutorial.component";
-import {LeaderBoardComponent} from '../leaderboard/leader-board.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-leader-board-button',
@@ -9,18 +7,9 @@ import {LeaderBoardComponent} from '../leaderboard/leader-board.component';
   styleUrls: ['./leader-board-button.component.scss'],
 })
 export class LeaderBoardButtonComponent {
-  constructor(private popoverController: PopoverController) { }
+  constructor(private router: Router) { }
 
-  async showPopover() {
-    const popover =  await this.popoverController.create({
-      component: LeaderBoardComponent,
-      translucent: true,
-      cssClass: 'tutorial-popover'
-    });
-    await popover.present();
-  }
-
-  openLeaderBoard() {
-    this.showPopover();
+  async openLeaderBoard() {
+    await this.router.navigateByUrl('leaderboard');
   }
 }
